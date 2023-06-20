bff35bdf995a7adff5b743ba5c73b5f7f0b7bcec
diwakar1593 <diwakar200kumar@gmail.com>
2023-06-20 14:57:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader';
+import style from './styles/modules/app.module.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <PageTitle>TODO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+        <AppHeader />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button, { SelectButton } from './Button';
+import styles from '../styles/modules/app.module.scss';
+
+function AppHeader() {
+  return (
+    <div className={styles.appHeader}>
+      <Button variant="primary">Click Me</Button>
+      <SelectButton id="status">
+        <option value="all">ALL</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Complete</option>
+      </SelectButton>
+    </div>
+  );
+}
+
+export default AppHeader;
