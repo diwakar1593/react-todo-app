bae5e4f09d20943c635d808c6ab796ac9648a612
diwakar1593 <diwakar200kumar@gmail.com>
2023-06-21 23:23:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
 import AppHeader from './components/AppHeader';
-import style from './styles/modules/app.module.scss';
+import styles from './styles/modules/app.module.scss';
+import AppContent from './components/AppContent';
 
 function App() {
   return (
     <div className="container">
       <PageTitle>TODO LIST</PageTitle>
-      <div className={style.app__wrapper}>
+      <div className={styles.app__wrapper}>
         <AppHeader />
+        <AppContent />
       </div>
     </div>
   );
